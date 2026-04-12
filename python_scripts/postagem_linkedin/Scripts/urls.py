8e97b68bd3e544578a6c8da92f863715fd40edd4
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-04-12 11:43:35 -0300

    postagem_linkedin

--- python_scripts/postagem_linkedin/Scripts/urls.py
+++ python_scripts/postagem_linkedin/Scripts/urls.py
@@ -1,44 +1,80 @@
 from sys import argv
+import json
+import ast
 
 class Urls_Array:
 
-    
-    # data = [  
-    #             { 
+    # data = [
+    #             {
     #                 "url": "https://businessweek.com.br/2026/04/03/inteligencia-artificial-reposiciona-rh-e-amplia-o-papel-humano-na-tomada-de-decisao/",
     #                 "id": 1,
-    #                 "createdAt": "2026-04-10T18:25:54.525Z", 
+    #                 "createdAt": "2026-04-10T18:25:54.525Z",
     #                 "updatedAt": "2026-04-10T18:25:54.525Z"
     #             }
     #       ]
 
-    def __init__(self,array):
-        
-        self.array = array
+    def convert_to_json(self, array):
+         
+        print("\nArray recebido: \n", array)
 
-        self.urls = []       
+        # converte string para dict Python
+        dados = ast.literal_eval(array)
+
+        # converte dict para JSON válido
+        self.json_valido_array = json.dumps(dados)
+        
+        print("\nJSON válido: \n", self.json_valido_array)
+
+        return self.json_valido_array
+    
+
+    def __init__(self, array):
+        
+        json_valido_array = self.convert_to_json(array)      
+
+        # converte JSON para lista de dicionários Python
+        self.array = json.loads(json_valido_array)        
+
+        self.urls = []
+
+        self.i = 1
 
 
     def get_urls(self):
+        for item in self.array:
+            print(f"Item {self.i}\n", item)
 
-        for url in self.array:
-            self.add_url(url['url'])
-    
+            self.add_url(item["url"])
 
-    def add_url(self,url):
+            self.i += 1
 
+    def add_url(self, url):
         self.urls.append(url)
-    
 
     def print_urls(self):
         print(self.urls)
-    
+
 
 if __name__ == "__main__":
-
-    urls_array = Urls_Array(argv[1]) # argv[1] é o argumento passado na linha de comando, que deve ser um array de URLs no formato JSON
     
-    urls_array.get_urls()
-    print(urls_array.urls)
+    # data = """[
+    #              {
+    #                  "url": "https://businessweek.com.br/2026/04/03/inteligencia-artificial-reposiciona-rh-e-amplia-o-papel-humano-na-tomada-de-decisao/",
+    #                  "id": 1,
+    #                  "createdAt": "2026-04-10T18:25:54.525Z",
+    #                  "updatedAt": "2026-04-10T18:25:54.525Z"
+    #              }
+    #         ]"""
 
-    
+    try:
+        if len(argv) < 2:
+            raise ValueError("Nenhum array recebido. Por favor, forneça um array JSON como argumento.")
+        
+        else:
+            urls_array = Urls_Array(argv[1]) # urls_array = Urls_Array(data)
+
+            urls_array.get_urls()
+            urls_array.print_urls()
+
+    except ValueError as e:
+        print(e)

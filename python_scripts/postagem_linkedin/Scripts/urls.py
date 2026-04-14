caa8c736dfb7c355180989081953c4a82e07e150
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-04-13 22:06:33 -0300

    postagem_linkedin

--- python_scripts/postagem_linkedin/Scripts/urls.py
+++ python_scripts/postagem_linkedin/Scripts/urls.py
@@ -1,30 +1,40 @@
 from sys import argv
+from re import sub
 import json
 
 
 class Urls_Array:
 
-    
-    def __init__(self, array:list):
+
+    def __init__(self, array: str):
         
-        # converte JSON para lista de dicionários Python
-        self.array = json.loads(array)        
-
-        self.urls = []
-
         self.i = 1
+        
+        raw = " ".join(array) # Concatena os elementos do array em uma única string
+        raw = sub(r'(?<=[{,])\s*(\w+)\s*:', r'"\1":', raw)   # Coloca aspas nas chaves
+        raw = sub(r':\s*([^",\}\]]+)', r': "\1"', raw)         # Coloca aspas nos valores
+        
+        self.array = json.loads(raw) # Converte a string JSON em um objeto Python
+
+        print("Array tratado: ", self.array)
+        
+        self.urls = []
 
 
     def get_urls(self):
+
         for item in self.array:
-            print(f"Item {self.i}\n", item)
+                        
+            print(f"Item {self.i}: ", item)
 
             self.add_url(item["url"])
 
             self.i += 1
 
+
     def add_url(self, url:str):
         self.urls.append(url)
+
 
     def print_urls(self) -> str:
         print(self.urls)
@@ -33,13 +43,11 @@ class Urls_Array:
 if __name__ == "__main__":
     
     try:
-        if len(argv) < 2: # Quantos args foram passados para o script? Se for menor que 2, 
-                          # significa que nenhum argumento foi fornecido (o primeiro argumento é sempre o nome do script)
-
-            raise ValueError("Nenhum argumento fornecido. Por favor, forneça um array de URLs como argumento.")
+        if len(argv) < 2:
+            raise ValueError("Nenhum array recebido. Por favor, forneça um array JSON como argumento.")
         
         else:
-            urls_array = Urls_Array(argv[1]) # Quem é argv[1] ? O argv[1] é o primeiro argumento passado para o script, que é o array de URLs.
+            urls_array = Urls_Array(argv[1:])
 
             urls_array.get_urls()
             urls_array.print_urls()

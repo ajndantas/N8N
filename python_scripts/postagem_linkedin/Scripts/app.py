ef30b3acf884cf5365135d92554cc920ab44d379
Antonio João Nascimento Dantas <antonio.dantas@gmail.com>
2026-04-15 01:08:41 -0300

    workflow

--- python_scripts/postagem_linkedin/Scripts/app.py
+++ python_scripts/postagem_linkedin/Scripts/app.py
@@ -1,8 +1,14 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
 from fastapi import FastAPI
 import subprocess
 
 from pydantic import BaseModel
 from typing import List
+
+from fastapi import Header, HTTPException, Depends
 
 # Este script é um aplicativo FastAPI que expõe um endpoint POST em "/urls".
 #
@@ -10,22 +16,32 @@ from typing import List
 # Você pode usar uma ferramenta como curl ou Postman para enviar uma solicitação POST para o endpoint "/urls" com um corpo de dados contendo o array de URLs.
 # 
 # Exemplo usando curl:
-# curl -X POST "https://antoniodantasn8n.ddns.net/urls" -H "Content-Type: application/json" -d "{\"urls\": [\"http://example.com\", \"http://example.org\"]}"
+# curl -X POST "https://sua-api/urls"  -H "Content-Type: application/json" -H "x-api-key: minha_chave_super_secreta" -d '{\"urls\": [\"http://example.com\", \"http://example.org\"]}'
 
 app = FastAPI()
+
+load_dotenv() # Carrega as variáveis de ambiente do arquivo .env
 
 class Input(BaseModel):
     data: List[dict] 
 
+
+# AUTENTICAÇÃO SIMPLES COM API KEY
+def verify_api_key(x_api_key: str = Header(...)):
+    if x_api_key != getenv("API_KEY"):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    
+
 @app.post("/urls")
-def run_script(input: Input):
+def run_script(input: Input, api_key: str = Depends(verify_api_key)):
     result = subprocess.run(
         ["python", "postagem_linkedin/Scripts/urls.py", str(input.data)], # O script "urls.py" deve estar no mesmo diretório que este arquivo "app.py"
         
         capture_output=True,
         text=True # Adiciona esta linha para capturar a saída como texto em vez de bytes
     )
-    
+
+        
     return {
         "stdout": result.stdout,
         "stderr": result.stderr
